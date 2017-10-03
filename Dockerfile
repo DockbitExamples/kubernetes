9ccb9688e5aacd31ae5eb24e6a4e6f53dc8b6fb6
Ahmed ElGamil <ahmed@dockbit.com>
2017-10-03 14:06:12 +0200

    Add 2 versions of app + Dockerfile with arguments

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM alpine:latest
 MAINTAINER The Dockbit Team "team@dockbit.com"
 
-COPY app/app /app
+ARG version=1.0
+COPY source/$version/app /app
 EXPOSE 8080
 ENTRYPOINT ["/app"]
---
+++ source/2.0/app.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const version string = "2.0"
+
+func getFrontpage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Congratulations! Version %s of your application is running on Kubernetes.", version)
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func getVersion(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s\n", version)
+}
+
+func main() {
+	http.HandleFunc("/", getFrontpage)
+	http.HandleFunc("/health", health)
+	http.HandleFunc("/version", getVersion)
+	http.ListenAndServe(":8080", nil)
+}
